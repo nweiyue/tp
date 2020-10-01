a667d42f414718f6da22cfa91f89156903f667e7
nweiyue <weiyue16@gmail.com>
2020-10-02 02:06:46 +0800

    Switch: Add more test cases

--- src/test/java/seedu/address/logic/commands/SwitchCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SwitchCommandTest.java
@@ -22,7 +22,7 @@ public class SwitchCommandTest {
     private Model expectedModel = new ModelManager();
 
     @Test
-    public void execute_validTab_success() {
+    public void execute_switchClassesTab_success() {
         String expectedMessage = String.format(MESSAGE_SWITCH_TAB_SUCCESS,
                 Tab.CLASSES.toString().toLowerCase());
         CommandResult expectedCommandResult = new CommandResult(expectedMessage, false, Tab.CLASSES, false);
@@ -32,7 +32,17 @@ public class SwitchCommandTest {
     }
 
     @Test
-    public void execute_invalidTab_throwsCommandException() {
+    public void execute_switchAttendanceTab_success() {
+        String expectedMessage = String.format(MESSAGE_SWITCH_TAB_SUCCESS,
+                Tab.ATTENDANCE.toString().toLowerCase());
+        CommandResult expectedCommandResult = new CommandResult(expectedMessage, false, Tab.ATTENDANCE, false);
+        SwitchCommand switchCommand = new SwitchCommand(ATTENDANCE_TAB_NAME);
+
+        assertCommandSuccess(switchCommand, model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_invalidTabName_throwsCommandException() {
         SwitchCommand switchCommand = new SwitchCommand(INVALID_TAB_NAME);
 
         assertCommandFailure(switchCommand, model, MESSAGE_INVALID_TAB);
@@ -40,23 +50,33 @@ public class SwitchCommandTest {
 
     @Test
     public void equals() {
-        SwitchCommand switchFirstCommand = new SwitchCommand(CLASSES_TAB_NAME);
-        SwitchCommand switchSecondCommand = new SwitchCommand(ATTENDANCE_TAB_NAME);
+        SwitchCommand switchToClassesCommand = new SwitchCommand(CLASSES_TAB_NAME);
+        SwitchCommand switchToAttendanceCommand = new SwitchCommand(ATTENDANCE_TAB_NAME);
+        SwitchCommand switchToInvalidTabCommand = new SwitchCommand(INVALID_TAB_NAME);
 
         // same object -> returns true
-        assertTrue(switchFirstCommand.equals(switchFirstCommand));
+        assertTrue(switchToClassesCommand.equals(switchToClassesCommand));
+        assertTrue(switchToAttendanceCommand.equals(switchToAttendanceCommand));
 
         // same values -> returns true
-        SwitchCommand switchCommandCopy = new SwitchCommand(CLASSES_TAB_NAME);
-        assertTrue(switchFirstCommand.equals(switchCommandCopy));
+        SwitchCommand switchToClassesCommandCopy = new SwitchCommand(CLASSES_TAB_NAME);
+        assertTrue(switchToClassesCommand.equals(switchToClassesCommandCopy));
+        SwitchCommand switchToAttendanceCommandCopy = new SwitchCommand(ATTENDANCE_TAB_NAME);
+        assertTrue(switchToAttendanceCommand.equals(switchToAttendanceCommandCopy));
 
         // different types -> returns false
-        assertFalse(switchFirstCommand.equals(1));
+        assertFalse(switchToClassesCommand.equals(1));
+        assertFalse(switchToAttendanceCommand.equals(1));
 
         // null -> returns false
-        assertFalse(switchFirstCommand.equals(null));
+        assertFalse(switchToClassesCommand.equals(null));
+        assertFalse(switchToAttendanceCommand.equals(null));
+
+        // invalid tabs -> returns false
+        assertFalse(switchToClassesCommand.equals(switchToInvalidTabCommand));
+        assertFalse(switchToAttendanceCommand.equals(switchToInvalidTabCommand));
 
         // different tabs -> returns false
-        assertFalse(switchFirstCommand.equals(switchSecondCommand));
+        assertFalse(switchToClassesCommand.equals(switchToAttendanceCommand));
     }
 }
--- src/test/java/seedu/address/logic/parser/SwitchCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/SwitchCommandParserTest.java
@@ -3,6 +3,7 @@ package seedu.address.logic.parser;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalTabNames.ATTENDANCE_TAB_NAME;
 import static seedu.address.testutil.TypicalTabNames.CLASSES_TAB_NAME;
 
 import org.junit.jupiter.api.Test;
@@ -15,16 +16,28 @@ public class SwitchCommandParserTest {
 
     @Test
     public void parse_emptyArg_throwsParseException() {
-        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, SwitchCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SwitchCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_whiteSpaces_throwsParseException() {
+        assertParseFailure(parser, "  \n\t   \n \t",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, SwitchCommand.MESSAGE_USAGE));
     }
 
     @Test
     public void parse_validArgs_returnsFindCommand() {
         // no leading and trailing whitespaces
-        SwitchCommand expectedSwitchCommand = new SwitchCommand(CLASSES_TAB_NAME);
-        assertParseSuccess(parser, CLASSES_TAB_NAME, expectedSwitchCommand);
+        SwitchCommand expectedSwitchToClassesCommand = new SwitchCommand(CLASSES_TAB_NAME);
+        assertParseSuccess(parser, CLASSES_TAB_NAME, expectedSwitchToClassesCommand);
+        SwitchCommand expectedSwitchToAttendanceCommand = new SwitchCommand(ATTENDANCE_TAB_NAME);
+        assertParseSuccess(parser, ATTENDANCE_TAB_NAME, expectedSwitchToAttendanceCommand);
 
         // leading and trailing whitespaces
-        assertParseSuccess(parser, " \n \t " + CLASSES_TAB_NAME + " \n \t ", expectedSwitchCommand);
+        assertParseSuccess(parser, " \n \t " + CLASSES_TAB_NAME + " \n \t ",
+                expectedSwitchToClassesCommand);
+        assertParseSuccess(parser, " \n \t " + ATTENDANCE_TAB_NAME + " \n \t ",
+                expectedSwitchToAttendanceCommand);
     }
 }
