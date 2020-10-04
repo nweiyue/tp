e7fdb228785ba45bb84f878ce1c48adff075b204
erisjacey <eris_jacey@Hotmail.com>
2020-10-04 18:58:48 +0800

    Messages.java: remove trailing whitespace

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -13,7 +13,6 @@ public class Messages {
     public static final String MESSAGE_MULTIPLE_STUDENTS_LISTED_OVERVIEW = "%1$d students listed!";
     public static final String MESSAGE_SINGLE_STUDENT_LISTED_OVERVIEW = "%1$d student listed!";
     public static final String MESSAGE_INVALID_CONFIRMATION_INPUT = "Confirmation input is invalid!";
-
     /**
      * Retrieves the correct message to be displayed to the user
      * based on the number of students in the list.
@@ -21,7 +20,6 @@ public class Messages {
      * @param numberOfStudents Number of students in the list.
      * @return message to be displayed to user.
      */
-
     public static String getStudentListedMessage(int numberOfStudents) {
         if (numberOfStudents == 1) {
             return MESSAGE_SINGLE_STUDENT_LISTED_OVERVIEW;
