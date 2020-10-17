364e1936ad6d786aff195aa8374a34a3b41a7849
nweiyue <weiyue16@gmail.com>
2020-10-17 17:34:41 +0800

    Remove duplicate files

--- src/test/java/atas/testutil/TypicalMemoContents 2.java
+++
@@ -1,7 +0,0 @@
-package atas.testutil;
-
-public class TypicalMemoContents {
-    public static final String EMPTY_MEMO_CONTENT = "";
-    public static final String SAMPLE_MEMO_CONTENT_ONE = "sample content one";
-    public static final String SAMPLE_MEMO_CONTENT_TWO = "sample content two";
-}
--- src/test/java/atas/testutil/TypicalMemos 2.java
+++
@@ -1,8 +0,0 @@
-package atas.testutil;
-
-import atas.model.memo.Memo;
-
-public class TypicalMemos {
-    public static final Memo SAMPLE_MEMO_ONE = new MemoBuilder().withConTent("memo 1").build();
-    public static final Memo SAMPLE_MEMO_TWO = new MemoBuilder().withConTent("memo 2").build();
-}
